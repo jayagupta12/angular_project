05738d49464da4a134a150d7a867ce241dc2c0fd
jaya-sysquo <jaya.sysquo@outlook.com>
2021-06-07 21:02:44 +0530

    chnages

--- src/app/questionpage/questionpage.component.ts
+++ src/app/questionpage/questionpage.component.ts
@@ -21,7 +21,7 @@ export class QuestionpageComponent implements OnInit {
   question: Question[];
   selectedAnswer: string;
   username: string;
-  friendname:string
+  friendname: string
   score: number = 0
   constructor(private router: Router,
     private httpService: NetworkcallsService,
@@ -31,21 +31,23 @@ export class QuestionpageComponent implements OnInit {
   ) {
     console.log(this.router.getCurrentNavigation().extras.state)
     this.username = (this.router.getCurrentNavigation().extras.state).toString();
-   
+
   }
   current: number = 0;
   btnText: string = "Next"
   link: string;
-   flag:number=0
+  flag: number = 0
   friendId: string
   ngOnInit(): void {
 
-console.log("username :", this.username);
+    console.log("username :", this.username);
+
     //friend part
     this.route.paramMap.subscribe(params => {
       this.friendId = params.get('id');
       console.log(this.friendId);
     });
+
     if (this.friendId != null) {
 
       var data = {
@@ -66,7 +68,7 @@ console.log("username :", this.username);
       })
     }
     else {
-
+// get question 
       this.httpService.getQuestion().subscribe((data: []) => {
         var a: any = data
         console.log(data)
@@ -83,44 +85,51 @@ console.log("username :", this.username);
 
     }
   }
+
+  // increase question index
   increaseindex() {
     var count = this.question.length
     if (count > this.current && this.btnText != "Submit") {
       this.current++
       this.btnText = "Next"
     }
-    if (this.current == this.question.length-1) {
+    if (this.current == this.question.length - 1) {
       this.btnText = "Submit"
     }
-    
-    if (this.btnText == "Submit" && this.flag==1) {
+
+    if (this.btnText == "Submit" && this.flag == 1) {
       console.log(this.question);
 
 
       if (this.friendId != null) {
 
+        //payload for friend score
         var data = {
           name: this.username,
           score: this.score.toString(),
           quizId: this.friendId
 
         }
+        // save friend score
         this.httpService.saveFriendScore(data).subscribe(data => {
 
-          console.log(data) // kya deskh rhi ho backtick lag
+          console.log(data)
           this.router.navigate([`/dashboard/${this.friendId}`]);
         })
 
       } else {
-        // Save this to db
+
+        //payload for question ganerated by new user
         const data = {
           name: this.username,
           question: this.question
         }
+
+        //save user question 
         this.httpService.saveUserQuestion(data).subscribe(data => {
           console.log(data)
           let id = data["id"]
-          this.link = "http://localhost:4200/dashboard/" + data["id"];
+          this.link = window.location.href+"/dashboard/" + data["id"];
 
           const dialogRef = this.dialog.open(ShareLinkdialogComponent, {
             width: '250px',
@@ -135,10 +144,10 @@ console.log("username :", this.username);
         })
       }
     }
- 
-    
-  }
 
+
+  }
+  // radio change event
   radioChange(event: MatRadioChange) {
     console.log(event.value);
     console.log(this.selectedAnswer);
@@ -151,8 +160,9 @@ console.log("username :", this.username);
       this.question[this.current].ans = this.selectedAnswer;
     }
 
-    if(this.current==this.question.length-1){
-this.flag=1
+    //if it is last question than submit flag
+    if (this.current == this.question.length - 1) {
+      this.flag = 1
     }
   }
 
